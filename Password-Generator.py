9cb16e77b0c592e1030d1a13095984e62a7c5000
Sami Islam <islam.samipkr@gmail.com>
2022-11-24 21:26:05 -0500

    Update Password-Generator.py

--- Password-Generator.py
+++ Password-Generator.py
@@ -2,7 +2,7 @@
 import random
 letter_list = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 number_list = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-symbol_list = ['!', '#', '$', '%', '&', '(', ')', '*', '+']  
+symbol_list = ['!', '#', '$', '%', '&', '(', ')', '*', '+']    
 
 total=int(input("How many total characters do you want in the password?"))
 numbers=int(input("How many numbers do you want in the password?"))
